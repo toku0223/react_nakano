10604a6b78524879e5587a6f37dab3b11fba7786
toku0223 <kei.nakanotoku@gmail.com>
2022-02-11 10:21:18 +0900

    課題２終了

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World
         </p>
-      </header>
-    </div>
   );
 }
 
