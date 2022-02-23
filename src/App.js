92801f29c13f20e516179441125c5eb3c93fdcc3
toku0223 <kei.nakanotoku@gmail.com>
2022-02-23 10:26:32 +0900

    main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   const buttonAlert = () => {
     window.alert("Hello World")
   }
+
+  const name = 'nakano';
+
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
+
   return (
-        <button onClick={buttonAlert}>
-            Hello World
-        </button>
+    <div>
+      <button onClick={buttonAlert}>
+        Hello World
+      </button>
+
+      <h1>Hello world</h1>
+      <h2>Hello world</h2>
+      <h3>Hello world</h3>
+      <h4>Hello world</h4>
+
+      <button>button</button>
+      <input type="text" />
+      <a href="#">a タグ</a>
+
+      <button onClick={increment}>いいね</button>
+      {count}
+
+      {name}
+
+    </div>
   );
 }
 
