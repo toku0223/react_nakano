828dd4007b30676b461d1e46f07cb29d9c575ded
toku0223 <kei.nakanotoku@gmail.com>
2022-02-26 09:52:10 +0900

    main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { useState } from 'react';
 import './App.css';
+import Card from './Card';
+
+const sampleArray = [
+  'aaa', 'bbb', 'ccc', 'ddd', 'eee'
+]
 
 function App() {
+
   const buttonAlert = () => {
     window.alert("Hello World")
   }
@@ -35,33 +41,55 @@ function App() {
     console.log(name)
   }
 
+  const sampleArray = [
+    { id: 1, name: 'aaa', date: 19920527 },
+    { id: 2, name: 'bbb', date: 19920528 },
+    { id: 3, name: 'ccc', date: 19920529 },
+    { id: 4, name: 'ddd', date: 19920531 },
+    { id: 5, name: 'eee', date: 19920501 }
+  ];
+
   return (
     <div>
-      <button onClick={buttonAlert}>
-        Hello World
-      </button>
 
-      <h1>Hello world</h1>
-      <h2>Hello world</h2>
-      <h3>Hello world</h3>
-      <h4>Hello world</h4>
+      <div>
+        {sampleArray.map((data, index) => {
+          console.log(index)
+          return <Card name={data.name} date={data.date} key={index} />;
+        })}
 
-      <button>button</button>
-      <input type="text" />
-      <a href="#">a タグ</a>
-      <br></br>
-      <button onClick={increment}>いいね！</button>
-      {count}
-      <button onClick={decrement}>よくないね！</button>
-      <br />
-      <button onClick={reset}>リセット</button>
-      <br />
-      <button onClick={nameplus}>add</button>
-      <br />
-      <button onClick={namereset}>addreset</button>
-      <br />
-      {name}
+        <Card name="nakano" date="2022/02/26" />
+      </div>
 
+
+
+      <div>
+        <button onClick={buttonAlert}>
+          Hello World
+        </button>
+
+        <h1>Hello world</h1>
+        <h2>Hello world</h2>
+        <h3>Hello world</h3>
+        <h4>Hello world</h4>
+
+        <button>button</button>
+        <input type="text" />
+        <a href="#">a タグ</a>
+        <br></br>
+        <button onClick={increment}>いいね！</button>
+        {count}
+        <button onClick={decrement}>よくないね！</button>
+        <br />
+        <button onClick={reset}>リセット</button>
+        <br />
+        <button onClick={nameplus}>add</button>
+        <br />
+        <button onClick={namereset}>addreset</button>
+        <br />
+        {name}
+
+      </div>
     </div>
   );
 }
