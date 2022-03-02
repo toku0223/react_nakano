d159d6efd64de57d602819e66af0828ae17e3db9
toku0223 <kei.nakanotoku@gmail.com>
2022-03-03 02:53:59 +0900

    main

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import ComponentA from './ComponentA';
 import ComponentB from './ComponentB';
 import ComponentC from './ComponentC';
 
 
+
 const App = () => {
+
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ComponentA />} />
-        <Route path="/componentb" element={<ComponentB />} />
-        <Route path="/componentc" element={<ComponentC />} />
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/componenta" element={<ComponentA />} />
+          <Route path="/componentb" element={<ComponentB />} />
+          <Route path="/componentc" element={<ComponentC />} />
+        </Routes>
+      </Router>
+    </div>
   );
 };
 
