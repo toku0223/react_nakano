bb6f2ce4971b8a2e6e83cdb4a6925d2fff530814
SatakeYusuke19920527 <satkyusk@icloud.com>
2022-03-03 13:14:01 +0900

    review

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 - [react_nakano](https://github.com/toku0223/react_nakano)
 
 ## proAca
-- stage-1
+- stage-1 fin
+- stage-2 fin
+- stage-3 fin
+- stage-4 now
--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import ComponentA from './ComponentA';
 import ComponentB from './ComponentB';
 import ComponentC from './ComponentC';
 
-
-
 const App = () => {
-
-
   return (
     <div>
       <Router>
